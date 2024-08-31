6ab83e76f82a1e99f10dee30370abe3a63640a7b
Induwara <induwara2k@gmail.com>
2024-09-01 00:46:21 +0530

    feat: implement nav-bar

--- src/components/WelcomeSection.vue
+++ src/components/WelcomeSection.vue
@@ -1,23 +1,52 @@
 <script>
+import { MessageCircleMore } from 'lucide-vue-next'
+import { AlignJustify } from 'lucide-vue-next'
 
-
-
+export default {
+  components: {
+    MessageCircleMore,
+    AlignJustify
+  }
+}
 </script>
 
-
-
 <template>
+  <div class="flex min-h-screen flex-col p-4 lg:p-6">
+    <div class="bg-welcome flex-grow rounded-3xl bg-cover bg-center">
+      <nav
+        class="mx-auto my-10 flex h-16 w-11/12 items-center justify-between rounded-lg bg-white p-5 md:h-20 lg:shadow-xl"
+      >
+        <div class="w-40 sm:w-52 lg:w-64">
+          <img src="/src/assets/images/navlogo.png" alt="Company Logo" />
+        </div>
 
-    <div>
+        <button class="lg:hidden" aria-label="Toggle menu">
+          <AlignJustify />
+        </button>
 
-
-
-
+        <div class="hidden lg:block">
+          <ul class="flex items-center space-x-10 text-blue-600">
+            <li><a href="#services" class="hover:text-blue-800">Services</a></li>
+            <li><a href="#lenses" class="hover:text-blue-800">Lenses</a></li>
+            <li><a href="#brands" class="hover:text-blue-800">Brands</a></li>
+            <li>
+              <a
+                href="#contact"
+                class="flex items-center justify-center gap-2 rounded-full bg-blue-600 px-8 py-2 text-sm text-white transition-colors hover:bg-blue-700"
+              >
+                Contact Us
+                <MessageCircleMore :size="20" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
-
-
+  </div>
 </template>
 
-
-
-<style scoped></style>
+<style scoped>
+.bg-welcome {
+  background-image: url(/src/assets/images/hero1.png);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-
+import './assets/style/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
