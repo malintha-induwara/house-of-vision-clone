7e6da7b5f48eb53c5aba07d213b5131ff0774537
Induwara <induwara2k@gmail.com>
2024-09-10 22:51:23 +0530

    feat: impelement who we are

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import WelcomeSection from '@/components/WelcomeSection.vue'
+import WhoWeAre from '@/components/WhoWeAre.vue'
 </script>
 
 <template>
   <main>
     <WelcomeSection />
+    <WhoWeAre />
   </main>
 </template>
 
---
+++ src/components/WhoWeAre.vue
@@ -0,0 +1,32 @@
+<script>
+
+
+
+
+</script>
+
+
+
+
+
+<template>
+    <div class="grid grid-cols-1 mx-5 gap-4 px-4 py-8 sm:grid-cols-2 lg:mx-24 lg:my-16 sm:gap-12">
+      <div class="flex pt-10 lg:gap-5 flex-col  items-start">
+        <h3 class="px-4  mb-4 text-sm font-semibold rounded-xl text-blue-700 bg-blue-400/40 sm:text-base md:text-lg lg:text-xl">
+          Who We Are
+        </h3>
+        <p class="mb-4  max-sm:text-3xl text-xl text-gray-700 xl:text-4xl">
+          Providing <span class="text-blue-600">expert eye care</span> with cutting edge technology and a curated selection of <span class="text-blue-600">premium
+          eyewear</span> ensuring you and your loved ones have access to the best eye care and eyewear options
+          available.
+        </p>
+      </div>
+      <div class="flex justify-center items-center sm:py-0 ">
+        <img src="/src/assets/images/aboutus.png" alt="aboutus">
+      </div>
+    </div>
+  </template>
+
+
+
+<style scoped></style>
